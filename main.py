acdf1d947a5807398a69e32e99a7c7f3fe4c93b7
MaxBenches <113772275+MaxBenches@users.noreply.github.com>
2023-09-20 16:54:16 +0200

    .

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import UDP_pc
-import time
 import controller
 
 while True:
     left_trig, right_trig, x_axis_left, button_x, button_y = controller.get_axis()
-    message = str(left_trig) + ":" + str(right_trig) + ":" + str(x_axis_left) + ":" + str(button_x) + ":" + str(button_y)
+    message = str(left_trig) + ":" + str(right_trig) + ":" + \
+              str(x_axis_left) + ":" + str(button_x) + ":" + str(button_y)
+
     print(message)
-    #UDP_pc.msg_send(message, "10.120.0.83", 5005)
+    UDP_pc.msg_send(message, "192.168.0.166", 5005)
