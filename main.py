88cec0fbf926bba92990b02557b7b3ad3ca3f115
MaxBenches <113772275+MaxBenches@users.noreply.github.com>
2023-09-21 19:22:11 +0200

    .

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import controller
 
 while True:
     trig_left, trig_right, x_axis_left, button_x, button_y, button_b = controller.get_axis()
-    message = str(trig_left) + ":" + str(trig_right) + ":" + \
-              str(x_axis_left) + ":" + str(button_x) + ":" + \
-              str(button_y) + ":" + str(button_b)
+    message = str(trig_left) + ":" + \
+              str(trig_right) + ":" + \
+              str(x_axis_left) + ":" + \
+              str(button_x) + ":" + \
+              str(button_y) + ":" + \
+              str(button_b)
 
     print(trig_left, trig_right)
-    UDP_pc.msg_send(message, "192.168.0.166", 5005)
+    UDP_pc.msg_send(message, "10.120.0.83", 5005)
